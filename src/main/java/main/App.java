f01fe32e83ec67291528a6f3834f95f0d0565314
SprinklerAi <157323843+SprinklerAi@users.noreply.github.com>
2024-02-02 22:20:13 +0200

    commit

---
+++ src/main/java/main/App.java
@@ -0,0 +1,66 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+
+ */
+
+public class App 
+{
+    public static void main( String[] args ) {
+
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<Animal> animals = new ArrayList<>();
+
+        boolean exit = false;
+        
+        System.out.println("Anna eläintarhalle nimi:");
+        Animal.set_zoo(sc.nextLine());
+
+        while (!exit) {
+            System.err.println("1) Luo uusi eläin, 2) Listaa kaikki eläimet, 3) Juoksuta eläimiä, 0) Lopeta ohjelma");
+            
+            if(sc.hasNextInt()) {
+                int choice = sc.nextInt();
+  
+                switch (choice) {
+                    case 1:
+                        System.out.println("Mikä laji?");
+                        String species = sc.next();
+                        
+                        System.out.println("Anna eläimen nimi: ");
+                        String animal_name = sc.next();
+
+                        System.out.println("Anna eläimen ikä: ");
+                        int age = Integer.parseInt(sc.next());
+
+                        animals.add(new Animal(animal_name, species, age));
+                        break;
+
+                    case 2:
+                        Animal.list_animals();
+                        break;
+                    case 3:
+                        System.out.println("Kuinka monta kierrosta?");
+                        sc.nextLine();
+                        int x = Integer.parseInt(sc.nextLine());
+                        Animal.run(x);
+                        break;
+                    
+                    case 0:
+                        System.out.println("Kiitos ohjelman käytöstä.");
+                        exit = true;
+                        break;
+
+                    default:
+                        System.out.println("Syöte oli väärä.");
+                        break;
+                }
+
+            }
+        }
+    }
+}
